ec039d9254f9f0084e3d08da22a6bbdb6b9328f2
Juliane Nagao <julianens@gmail.com>
2020-03-19 17:11:27 +0100

    feat: test ProductList

---
+++ src/components/ProductList/ProductList.test.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { ProductList } from 'components';
+
+describe('ProductList', () => {
+  let wrapper;
+
+  const productsMock = [
+    {
+      id: 'coke',
+      name: 'coke',
+      img: 'image/coke.png',
+      price: 1,
+      currentStock: 10,
+    },
+    {
+      id: 'sprite',
+      name: 'sprite',
+      img: 'image/sprite.png',
+      price: 1.25,
+      currentStock: 5,
+    },
+    {
+      id: 'fanta',
+      name: 'fanta',
+      img: 'image/fanta.png',
+      price: 1.25,
+      currentStock: 11,
+    },
+  ];
+
+  const onSelectProductMock = jest.fn();
+
+  beforeEach(() => {
+    wrapper = shallow(
+      <ProductList
+        products={productsMock}
+        onSelectProduct={onSelectProductMock}
+      />,
+    );
+  });
+
+  it('should render correctly given the required props', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should render one unordered list ProductListUl element', () => {
+    expect(wrapper.find('ProductListUl').length).toEqual(1);
+  });
+
+  it('should render 3 instances of <ProductItem />', () => {
+    expect(wrapper.find('ProductItem').length).toEqual(3);
+  });
+});
